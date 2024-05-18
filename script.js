6c232e773d02bbbde9b17fc718a862d3d5ac73b6
Clifford Anderson <c.andersoninfotech@gmail.com>
2024-05-18 10:38:42 -0500

    ran some commands in the console, and found out how to grab the API

---
+++ script.js
@@ -0,0 +1,16 @@
+
+async function getQuote() {
+
+  const test = document.getElementById(`test`);
+
+  try {
+    const resp = await fetch('https://api.adviceslip.com/advice');
+    const data = await resp.json();
+    // test.innerHTML = data.slip.advice
+  }
+  catch(err) {
+    console.error('Error: Unable to reach resource', err)
+  }
+}
+
+getQuote()
