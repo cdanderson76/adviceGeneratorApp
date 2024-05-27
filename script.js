8ff42472ebd35f7e0035d7f0a6f0a1f55fad8813
Clifford Anderson <c.andersoninfotech@gmail.com>
2024-05-27 18:14:40 -0500

    made some animation changes in referencet to how a new quote loads

--- script.js
+++ script.js
@@ -5,19 +5,33 @@ const diceBtn = document.getElementById('dice-container');
 
 async function getQuote() {
 
-  const test = document.getElementById(`test`);
-
   try {
     const resp = await fetch('https://api.adviceslip.com/advice');
     const data = await resp.json();
 
     advice.textContent = `Advice #${data.slip.id}`;
-    quote.textContent = `" ${data.slip.advice}"`;
+    quote.textContent = `"${data.slip.advice}"`;
+
+    if(data.slip.advice.length >= 20 ) {
+      quote.style.fontSize = '20px';
+    }
 
     diceBtn.addEventListener('click', function() {
-      location.reload();
+
+      location.reload;
+
+      advice.textContent = `Advice #${data.slip.id}`;
+      quote.textContent = `"${data.slip.advice}"`;
+
+      advice.textContent = '';
+      quote.innerHTML = 
+          '<img src="images/ripple.svg" alt="ripple loading effect" class="ripple">';
+      
+      setTimeout(getQuote, 2500)
+
     });
 
+    
   }
   catch(err) {
     console.error('Error: Unable to reach resource', err)
