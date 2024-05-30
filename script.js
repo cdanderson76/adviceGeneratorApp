791e8c524094b753401ec56f4ff8f745e1524433
cdanderson76 <138369806+cdanderson76@users.noreply.github.com>
2024-05-30 16:39:36 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ const quote = document.getElementById('quote');
 const diceBtn = document.getElementById('dice-container');
 const quoteCard = document.getElementById('quote-card');
 
+diceBtn.addEventListener('click', function() {
+
+  location.reload;
+
+  advice.textContent = '';
+  quote.innerHTML = 
+      '<img src="images/ripple.svg" alt="ripple loading effect" id="ripple">';
+  
+  setTimeout(getQuote, 2500)
+});
+
+// Async function to grab the data from the API
+
 async function getQuote() {
 
   try {
@@ -12,13 +25,9 @@ async function getQuote() {
     advice.textContent = `Advice #${data.slip.id}`;
     quote.textContent = `"${data.slip.advice}"`;
 
-    // advice.textContent;
-    // quote.textContent;
 
-    console.log(data.slip.advice.length);
-    console.log(quoteCard.getBoundingClientRect().width);
 
-    // GET YOUR DAMN CONDITIONALS TOGETHER!!!
+    // Conditionals that set my font size for the quotes
 
     if(data.slip.advice.length > 60) {
       quote.style.fontSize = '1.375rem';
@@ -31,18 +40,6 @@ async function getQuote() {
     else if( data.slip.advice.length < 35 ) {
       quote.style.fontSize = '1.75rem';
     }
-    
-    diceBtn.addEventListener('click', function() {
-
-      location.reload;
-
-      advice.textContent = '';
-      quote.innerHTML = 
-          '<img src="images/ripple.svg" alt="ripple loading effect" id="ripple">';
-      
-      setTimeout(getQuote, 2500)
-
-    });
   }
   catch(err) {
     console.error('Error: Unable to reach resource', err)
