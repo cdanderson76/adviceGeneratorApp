2b90bffea6626868079257b1269c11e5c08a28c3
Clifford Anderson <c.andersoninfotech@gmail.com>
2024-05-29 22:21:44 -0500

    modified conditionals for font size

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ const quote = document.getElementById('quote');
 const diceBtn = document.getElementById('dice-container');
 const quoteCard = document.getElementById('quote-card');
 
-
-console.log(quoteCard.getBoundingClientRect().width);
-
 async function getQuote() {
 
   try {
@@ -15,36 +12,39 @@ async function getQuote() {
     advice.textContent = `Advice #${data.slip.id}`;
     quote.textContent = `"${data.slip.advice}"`;
 
-    if(data.slip.advice.length >= 80) {
+    // advice.textContent;
+    // quote.textContent;
+
+    console.log(data.slip.advice.length);
+    console.log(quoteCard.getBoundingClientRect().width);
+
+    // GET YOUR DAMN CONDITIONALS TOGETHER!!!
+
+    if(data.slip.advice.length > 70) {
       quote.style.fontSize = '22px';
     }
-    // else if( data.slip.advice.length < 80 ) {
-    //   quote.style.fontSize = '28px';
-    // }
-
-    // && quoteCard.getBoundingClientRect().width === '540px'
-    
+    else if( data.slip.advice.length < 70 && quoteCard.getBoundingClientRect().width > '500px') {
+      quote.style.fontSize = '24px';
+    }
+    else if( data.slip.advice.length < 70 && quoteCard.getBoundingClientRect().width > '600px') {
+      quote.style.fontSize = '28px'
+    }
 
     diceBtn.addEventListener('click', function() {
 
       location.reload;
 
-      advice.textContent;
-      quote.textContent;
-
       advice.textContent = '';
       quote.innerHTML = 
-          '<img src="images/ripple.svg" alt="ripple loading effect" class="ripple">';
+          '<img src="images/ripple.svg" alt="ripple loading effect" id="ripple">';
       
       setTimeout(getQuote, 2500)
 
     });
-
-    
   }
   catch(err) {
     console.error('Error: Unable to reach resource', err)
   }
 }
 
-getQuote()
+getQuote();
